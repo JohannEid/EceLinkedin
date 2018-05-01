4435badf4f18d16a21d8af17942ac4326b9459dd
Clément de Bailliencourt <clementdebailliencourt@gmail.com>
2018-05-01 11:41:37 +0200

    Creation des php files

---
+++ php/signin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: clementdebailliencourt
+ * Date: 5/1/18
+ * Time: 11:40 AM
+ */
+
+$email = isset($_POST["email"])? $_POST["email"] : "";
+$password = isset($_POST["password"])? $_POST["password"] : "";
---
+++ php/signup.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: clementdebailliencourt
+ * Date: 5/1/18
+ * Time: 11:28 AM
+ */
+
+$fname = isset($_POST["fname"])? $_POST["fname"] : "";
+$lname = isset($_POST["lname"])? $_POST["lname"] : "";
+$email = isset($_POST["email"])? $_POST["email"] : "";
+$username = isset($_POST["username"])? $_POST["username"] : "";
+$password = isset($_POST["password"])? $_POST["password"] : "";
