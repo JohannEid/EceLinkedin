d5971de4329780fc21f15550687611cdeb7b79c2
JohannEid <jojoking97@gmail.com  git config --global user.name SOATRAP git commit -m Add>
2018-05-02 14:19:23 +0200

    added sign php file

--- php/Config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_SERVER', 'localhost:3036');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'MeetECE');
-$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
