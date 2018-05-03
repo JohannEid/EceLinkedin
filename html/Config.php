305e670d9e6a3c534db5d48d394ed00e1485293e
TOMTOM-COMPUTER\Thomas <thomas.cop--gaillot@edu.ece.fr>
2018-05-03 18:31:29 +0200

    Insertion d'user et d'un réseau manuellement

--- html/Config.php
+++ html/Config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'MeetECE');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'MeetEce');
 $db = mysqli_connect(DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 
